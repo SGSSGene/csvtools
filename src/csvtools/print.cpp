c369642103290b5e83f4fb91c116dbcbf58836a5
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2025-07-13 21:45:37 +0200

    feat: add filtering option

--- src/csvtools/print.cpp
+++ src/csvtools/print.cpp
@@ -66,7 +66,30 @@ auto cliCustomPrint = clice::Argument {
     .desc   = "custom format",
     .value  = std::vector<std::string>{},
 };
+auto cliFilterPrint = clice::Argument {
+    .parent = &cliCmd,
+    .args   = {"--filter"},
+    .desc   = "format if filter fits",
+    .value  = std::vector<std::string>{},
+};
+auto cliTransformPrint = clice::Argument {
+    .parent = &cliCmd,
+    .args   = {"--transform"},
+    .desc   = "transforms the value",
+    .value  = std::vector<std::string>{},
+};
 
+struct Rect {
+    size_t startRow{0}, endRow{std::numeric_limits<size_t>::max()};
+    size_t startCol{0}, endCol{std::numeric_limits<size_t>::max()};
+
+    bool isInRange(size_t row, size_t col) const {
+        bool valid = true;
+        valid = valid && startRow <= row && row <= endRow;
+        valid = valid && startCol <= col && col <= endCol;
+        return valid;
+    }
+};
 
 void printLatexTable(std::vector<std::string> entries) {
 }
@@ -89,7 +112,7 @@ void app() {
             width = std::max(width, values.back().size());
         }
 
-        // make table quadtratic
+        // make table quadratic
         for (size_t y{0}; y < values.size(); ++y) {
             values[y].resize(width);
         }
@@ -145,8 +168,194 @@ void app() {
             std::swap(vec, values);
         }
 
-        auto customFmt = std::unordered_map<size_t, std::string>{};
+        auto cachedValues = std::unordered_map<std::string, double>{};
+
+        auto cachedValue_fetch_or_run = [&](std::string id, auto cb) {
+            auto iter = cachedValues.find(id);
+            if (iter == cachedValues.end()) {
+                auto v = cb();
+                cachedValues[id] = v;
+                iter = cachedValues.find(id);
+            }
+            return iter->second;
+        };
+        auto cachedValue_fetch_or_cacc = [&](size_t col, size_t start, size_t end, auto init, auto bin) {
+            for (size_t i{start}; i <= end; ++i) {
+                auto& entries = values[i];
+                auto v = std::stod(entries[col]);
+                init = bin(v, init);
+            }
+            return init;
+        };
+
+        auto cachedValue_fetch_or_racc = [&](size_t row, size_t start, size_t end, auto init, auto bin) {
+            for (size_t i{start}; i < std::min(values[row].size(), end); ++i) {
+                auto const& e = values[row][i];
+                auto v = std::stod(e);
+                init = bin(v, init);
+            }
+            return init;
+        };
+
+        auto cachedValue_cmin = [&](size_t col, size_t start, size_t end) -> double {
+            return cachedValue_fetch_or_run(fmt::format("cmin{}:{}-{}", col, start, end), [&]() {
+                return cachedValue_fetch_or_cacc(col, start, end, std::numeric_limits<double>::max(), [](double lhs, double rhs) {
+                    return std::min(lhs, rhs);
+                });
+            });
+        };
+
+        auto cachedValue_cmax = [&](size_t col, size_t start, size_t end) -> double {
+            return cachedValue_fetch_or_run(fmt::format("cmax{}:{}-{}", col, start, end), [&]() {
+                return cachedValue_fetch_or_cacc(col, start, end, std::numeric_limits<double>::lowest(), [](double lhs, double rhs) {
+                    return std::max(lhs, rhs);
+                });
+            });
+        };
+
+        auto cachedValue_rmin = [&](size_t row, size_t start, size_t end) -> double {
+            return cachedValue_fetch_or_run(fmt::format("rmin{}:{}-{}", row, start, end), [&]() {
+                return cachedValue_fetch_or_racc(row, start, end, std::numeric_limits<double>::max(), [](double lhs, double rhs) {
+                    return std::min(lhs, rhs);
+                });
+            });
+        };
+
+        auto cachedValue_rmax = [&](size_t row, size_t start, size_t end) -> double {
+            return cachedValue_fetch_or_run(fmt::format("rmax{}:{}-{}", row, start, end), [&]() {
+                return cachedValue_fetch_or_racc(row, start, end, std::numeric_limits<double>::lowest(), [](double lhs, double rhs) {
+                    return std::max(lhs, rhs);
+                });
+            });
+        };
+
+        if (cliFilterPrint) {
+            using Filter = std::function<std::string(size_t row, size_t col, std::string)>;
+            auto filterMap = std::vector<std::tuple<Rect, Filter>>{};
+            for (auto l : *cliFilterPrint) {
+                auto parseNextF = [](std::string& s) {
+                    auto iter = s.find(":");
+                    if (iter == std::string::npos) {
+                        throw std::runtime_error{"filter must be of format: \"<rnr>:<cnr>:<filter>:<fmt>\""};
+                    }
+                    auto ret = s.substr(0, iter);
+                    s = s.substr(iter+1);
+                    return ret;
+                };
+                auto rowsStr   = parseNextF(l);
+                auto colsStr   = parseNextF(l);
+                auto filterStr = parseNextF(l);
+                auto suffix    = l;
+
+                // parse number
+                auto parseNumberRange = [](std::string s, size_t min, size_t max) -> std::tuple<size_t, size_t> {
+                    if (auto iter = s.find("-"); iter != std::string::npos) {
+                        auto start = s.substr(0, iter);
+                        size_t startI = min;
+                        auto end   = s.substr(iter+1);
+                        size_t endI = max;
+
+                        if (start.size()) {
+                            startI = std::stoi(start);
+                        }
+                        if (end.size()) {
+                            endI = std::stoi(end);
+                        }
+                        return {startI, endI};
+                    } else if (s.size()) {
+                        auto v = std::stoi(s);
+                        return {v, v};
+                    }
+                    return {0, max};
+                };
+
+
+                // parse which columns to apply this to
+                auto [rstart, rend] = parseNumberRange(rowsStr, 0, std::numeric_limits<size_t>::max());
+                auto [cstart, cend] = parseNumberRange(colsStr, 0, width-1);
+                auto rect = Rect {
+                    rstart, rend,
+                    cstart, cend,
+                };
+
+
+                auto filter = [=](size_t row, size_t col, std::string s) {
+                    if (filterStr == "true") {
+                        s = fmt::format(fmt::runtime(suffix), s);
+                    }
+                    if (filterStr == "cmin") {
+                        if (std::stod(s) == cachedValue_cmin(col, rstart, rend)) {
+                            s = fmt::format(fmt::runtime(suffix), s);
+                        }
+                    }
+                    if (filterStr == "cmax") {
+                        if (std::stod(s) == cachedValue_cmax(col, rstart, rend)) {
+                            s = fmt::format(fmt::runtime(suffix), s);
+                        }
+                    }
+                    if (filterStr.starts_with("cmin ")) {
+                        auto per = std::stod(filterStr.substr(5));
+                        if (std::stod(s)*per <= cachedValue_cmin(col, rstart, rend)) {
+                            s = fmt::format(fmt::runtime(suffix), s);
+                        }
+                    }
+                    if (filterStr.starts_with("cmax ")) {
+                        auto per = std::stod(filterStr.substr(5));
+                        if (std::stod(s) >= cachedValue_cmax(col, rstart, rend)*per) {
+                            s = fmt::format(fmt::runtime(suffix), s);
+                        }
+                    }
+                    if (filterStr == "rmin") {
+                        if (std::stod(s) == cachedValue_rmin(row, cstart, cend)) {
+                            s = fmt::format(fmt::runtime(suffix), s);
+                        }
+                    }
+                    if (filterStr == "rmax") {
+                        if (std::stod(s) == cachedValue_rmax(row, cstart, cend)) {
+                            s = fmt::format(fmt::runtime(suffix), s);
+                        }
+                    }
+                    if (filterStr.starts_with("rmin ")) {
+                        auto per = std::stod(filterStr.substr(5));
+                        if (std::stod(s)*per <= cachedValue_rmin(row, cstart, cend)) {
+                            s = fmt::format(fmt::runtime(suffix), s);
+                        }
+                    }
+                    if (filterStr.starts_with("rmax ")) {
+                        auto per = std::stod(filterStr.substr(5));
+                        if (std::stod(s) >= cachedValue_rmax(row, cstart, cend)*per) {
+                            s = fmt::format(fmt::runtime(suffix), s);
+                        }
+                    }
+                    if (filterStr == "float") {
+                        auto v = std::stod(s);
+                        s = fmt::format(fmt::runtime(suffix), v);
+                    }
+
+                    return s;
+                };
+
+                filterMap.emplace_back(rect, filter);
+            }
+
+            for (size_t row{0}; row < values.size(); ++row) {
+                auto& entries = values[row];
+                for (size_t col{0}; col < entries.size(); ++col) {
+                    auto& e = entries[col];
+                    for (auto [rect, filter] : filterMap) {
+                        if (!rect.isInRange(row, col)) continue;
+                        try {
+                            e = filter(row, col, e);
+                        } catch(...){}
+                    }
+                }
+            }
+        }
+
+
+
         if (cliCustomPrint) {
+            auto customFmt = std::unordered_map<size_t, std::string>{};
             for (auto l : *cliCustomPrint) {
                 auto iter = l.find(":");
                 if (iter == std::string::npos) {
